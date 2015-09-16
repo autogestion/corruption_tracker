16ab6a196191c61a9167bb72ed298ad91994ed69
mk <var@autogestion.ru>
2015-09-16 21:39:12 +0300

    add explanations for users

--- claim/views.py
+++ claim/views.py
@@ -8,28 +8,27 @@ from claim.models import Claim
 
 def get_claims(request, polygon_id):
     # results = {'success':False}
-    try:
-        claims = Claim.objects.filter(polygon_id=polygon_id)
 
-        if claims:
-            claims_list = []
-            for claim in claims:
-                details = {'text': claim.text, 'servant': claim.servant,
-                            'complainer': claim.complainer.username,}
-                claim_block = """Службовець: %(servant)s <br>
-                                 Скарга: %(text)s    <br>
-                                 Вiд: %(complainer)s   <br><br>""" %details
+    claims = Claim.objects.filter(polygon_id=polygon_id)
 
-                claims_list.append(claim_block)
-                
-            claims_html = ''.join(claims_list)
+    if claims:
+        claims_list = []
+        for claim in claims:
+            details = {'text': claim.text, 'servant': claim.servant,
+                        'complainer': claim.complainer.username,}
+            claim_block = """Службовець: %(servant)s <br>
+                             Скарга: %(text)s    <br>
+                             Вiд: %(complainer)s   <br><br>""" %details
 
-            results = {'success':True, 'claims': claims_html}
+            claims_list.append(claim_block)
+            
+        claims_html = ''.join(claims_list)
 
-        else:
-            results = {'success':True, 'claims': 'No claims for this polygon'}
-    except Exception as e:
-        print(e)
+        results = {'success':True, 'claims': claims_html}
+
+    else:
+        results = {'success':True, 'claims': 'На цей заклад немає скарг'}
+
 
     return HttpResponse(json.dumps(results), content_type='application/json')
 
