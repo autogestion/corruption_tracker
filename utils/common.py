3b3a898e1b7c1917c243d053742fd6cd501fb69d
Mykola Yakovliev <vegasq@gmail.com>
2015-09-20 11:56:42 +0300

    Python 2 support returned
    + Just tried to deploy proj on one of mine raspberry, and discovered
      that there is a bug that do not allow me install django 1.7/1.8
      based on python 3 to raspbian. Everything worked with OOB py2.7
      and this little fix.
    * If there some reason why we prefer to use Py3 nor Py2, please
      share it with me.

--- utils/common.py
+++ utils/common.py
@@ -1,4 +1,18 @@
+import json
 
+from django.conf import settings
+
+
+def read_map():
+    try:
+        # python 3+
+        json_s = open(settings.GEOJSON, encoding='utf8').read()
+    except TypeError:
+        # python 2
+        json_s = open(settings.GEOJSON).read()
+        json_s = json_s.decode('utf8')
+
+    return json.loads(json_s)
 
 
 # TODO(vegasq) Need create utils module, or something similar.
