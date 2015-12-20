22accbc74c2750d77ed3bbe65133b2615975163d
mk <var@autogestion.ru>
2015-12-20 17:56:11 +0200

    :fish: add api call to retrieve polygons hierarchy

--- geoinfo/serializers.py
+++ geoinfo/serializers.py
@@ -1,10 +1,38 @@
-from rest_framework import serializers
 
-from geoinfo.models import Layer, Polygon
+from rest_framework import serializers, generics
+from geoinfo.models import Polygon
 
 
 class PolygonSerializer(serializers.ModelSerializer):
+    # parentCategory = serializers.PrimaryKeyRelatedField()
 
     class Meta:
         model = Polygon
-        fields = ('organization_count')
+        # fields = ('subpolygons', 'polygon_id', 'address')
+        fields = ('polygon_id', 'address')
+
+# PolygonSerializer.base_fields['subpolygons'] = PolygonSerializer()
+
+
+class PolygonView(generics.ListAPIView):
+
+    model = Polygon
+    serializer_class = PolygonSerializer
+
+
+def extractor(polygon_id):
+    responce = {}
+    polygon = Polygon.objects.get(polygon_id=polygon_id)
+    if polygon.level == 4:
+        orgs = {x.id: [x.name, x.org_type] for x in polygon.organizations.all()}
+        responce[polygon.polygon_id] = {'address': polygon.address,
+                                        'orgs': orgs}
+        return responce
+    else:
+        responce[polygon.polygon_id] = {'address': polygon.address,
+                                        'childs': {}}
+        childs = polygon.polygon_set.all()
+        for child in childs:
+            responce[polygon.polygon_id]['childs'].update(extractor(child.polygon_id))
+
+        return responce
